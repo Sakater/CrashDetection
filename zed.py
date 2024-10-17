50668b3ca973d3e75380a9dc88941506995e8c3a
Yusuf Solmaz <yusuf.solmaz@th-wildau.de>
2024-10-17 13:49:30 +0200

    update4

--- zed.py
+++ zed.py
@@ -1,13 +1,17 @@
-# -*- coding: utf-8 -*-
+import cv2
 import rospy
+from cv_bridge import CvBridge
 from sensor_msgs.msg import Image
 from std_msgs.msg import Float32
-import cv2
-import numpy as np
-from cv_bridge import CvBridge
 
+# Constants
+MAX_STEERING_ANGLE = 30.0
+MAX_STEERING_VALUE = 100.0
 
-# Funktion zur Berechnung des Fahrschlauchs basierend auf dem Lenkwinkel
+# Initialize CvBridge
+bridge = CvBridge()
+
+# Function to calculate the ROI based on the steering angle
 def calculate_roi_based_on_steering(angle, image_width, image_height):
     roi_width = int(image_width / (1 + abs(angle) / 10))
     roi_height = int(image_height / 3)
@@ -19,33 +23,36 @@ def calculate_roi_based_on_steering(angle, image_width, image_height):
 
     return x_min, y_min, x_max, y_max
 
-
-# Funktion zur Berechnung des Lenkwinkels
+# Function to calculate the steering angle
 def calculate_steering_angle(steering_value):
     return (steering_value / MAX_STEERING_VALUE) * MAX_STEERING_ANGLE
 
-
-# Callback-Funktion für das Abonnieren des Lenkungswerts
+# Callback function for the steering value
 def steering_callback(msg):
+    global steering_angle
     steering_value = msg.data
     steering_angle = calculate_steering_angle(steering_value)
     rospy.loginfo("Lenkwinkel: {:.2f} Grad".format(steering_angle))
 
-
-# Funktion, um den Fahrschlauch basierend auf dem Lenkwinkel zu zeichnen
-def draw_fahrschlauch(image, angle, image_width, image_height):
+# Function to draw the driving lane and ROI on the image
+def draw_fahrschlauch_and_roi(image, angle, image_width, image_height):
     center_x = image_width // 2
     y_min = image_height // 2
     y_max = image_height
 
     offset = int(angle / MAX_STEERING_ANGLE * center_x)
 
+    # Draw the driving lane
     cv2.line(image, (center_x - offset, y_min), (center_x - offset, y_max), (0, 255, 0), 3)
     cv2.line(image, (center_x + offset, y_min), (center_x + offset, y_max), (0, 255, 0), 3)
+
+    # Calculate and draw the ROI
+    x_min, y_min, x_max, y_max = calculate_roi_based_on_steering(angle, image_width, image_height)
+    cv2.rectangle(image, (x_min, y_min), (x_max, y_max), (255, 0, 0), 2)
+
     return image
 
-
-# Callback-Funktion für das Bild
+# Callback function for the image
 def image_callback(msg):
     global steering_angle
 
@@ -53,43 +60,21 @@ def image_callback(msg):
 
     height, width, _ = frame.shape
 
-    frame_with_fahrschlauch = draw_fahrschlauch(frame, steering_angle, width, height)
+    frame_with_fahrschlauch_and_roi = draw_fahrschlauch_and_roi(frame, steering_angle, width, height)
 
-    cv2.imshow("ZED Camera with Fahrschlauch", frame_with_fahrschlauch)
+    cv2.imshow("ZED Camera with Fahrschlauch and ROI", frame_with_fahrschlauch_and_roi)
     cv2.waitKey(1)
 
-
-# Callback-Funktion für das Abonnieren der Tiefendaten
-def depth_callback(msg):
-    try:
-        depth_image = bridge.imgmsg_to_cv2(msg, desired_encoding="passthrough")
-
-        steering_angle = 15
-        image_height, image_width = depth_image.shape
-        x_min, y_min, x_max, y_max = calculate_roi_based_on_steering(steering_angle, image_width, image_height)
-
-        for x in range(x_min, x_max):
-            for y in range(y_min, y_max):
-                distance = depth_image[y, x]
-                if distance > 0:
-                    print("Objekt bei ({}, {}) hat eine Entfernung von: {} Meter".format(x, y, distance))
-
-    except Exception as e:
-        rospy.logerr("Fehler beim Verarbeiten der Tiefendaten: {}".format(e))
-
-
 def main():
+    global steering_angle
+    steering_angle = 0
+
     rospy.init_node('zed_fahrschlauch_analyse', anonymous=True)
 
-    rospy.Subscriber("/zed2/zed_node/depth/depth_registered", Image, depth_callback)
+    rospy.Subscriber("/zed2/zed_node/depth/depth_registered", Image, image_callback)
     rospy.Subscriber("/ctrlcmd_steering", Float32, steering_callback)
-    rospy.Subscriber("/zed2/zed_node/rgb/image_rect_color", Image, image_callback)
 
     rospy.spin()
 
-
 if __name__ == "__main__":
-    MAX_STEERING_ANGLE = 30.0
-    MAX_STEERING_VALUE = 100.0
-    bridge = CvBridge()
     main()
